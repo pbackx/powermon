551d8c66aab8d99f2e77c2c62035b8598549ad79
Peter Backx <peter.backx@gmail.com>
2023-01-07 11:26:11 +0100

    Add basic chart with power consumption

--- powermon-frontend/src/App.tsx
+++ powermon-frontend/src/App.tsx
@@ -1,62 +1,103 @@
 import React, {useEffect, useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import useWebSocket, {ReadyState} from 'react-use-websocket';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import {Line} from 'react-chartjs-2';
 
 function getWebsocketUrl(url: string) {
-  let websocketUrl
-  if (url.startsWith("http://")) {
-    websocketUrl = url.replace(/^http/, "ws")
-  } else {
-    const l = window.location
-    websocketUrl = "ws://" + l.host + l.pathname + url.replace(/^\.\//, "")
-  }
+    let websocketUrl
+    if (url.startsWith("http://")) {
+        websocketUrl = url.replace(/^http/, "ws")
+    } else {
+        const l = window.location
+        websocketUrl = "ws://" + l.host + l.pathname + url.replace(/^\.\//, "")
+    }
 
-  if (websocketUrl.endsWith("/")) {
-    return websocketUrl + "ws"
-  } else {
-    return websocketUrl + "/ws"
-  }
+    if (websocketUrl.endsWith("/")) {
+        return websocketUrl + "ws"
+    } else {
+        return websocketUrl + "/ws"
+    }
 }
 
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
 function App() {
-  const [message, setMessage] = useState("loading...")
+    const [message, setMessage] = useState("loading...")
+    const [labels, setLabels] = useState<string[]>([])
+    const [powerConsumption, setPowerConsumption] = useState<number[]>([])
 
-  const { sendMessage, lastMessage, readyState } = useWebSocket(getWebsocketUrl(process.env.REACT_APP_API_URL || ""))
-  useEffect(() => {
-    if (lastMessage !== null) {
-      console.log(JSON.parse(lastMessage.data))
+    const {sendMessage, lastMessage, readyState} = useWebSocket(getWebsocketUrl(process.env.REACT_APP_API_URL || ""))
+    useEffect(() => {
+        if (lastMessage !== null) {
+            const msg = JSON.parse(lastMessage.data)
+            console.log(`${msg.entity_id} ${msg.state}${msg.attributes.unit_of_measurement}`)
+
+            const time_formatted = new Date(msg.last_updated).toLocaleTimeString()
+            const value = parseFloat(msg.state)
+
+            setLabels([...labels, time_formatted])
+            setPowerConsumption([...powerConsumption, value])
+        }
+    }, [lastMessage])
+
+    useEffect(() => {
+        window.fetch(process.env.REACT_APP_API_URL || "")
+            .then(response => response.json())
+            .then(data => setMessage(data.message))
+            .catch(error => setMessage(`Error: ${error}`))
+    }, [setMessage])
+
+    const connectionStatus = {
+        [ReadyState.CONNECTING]: 'Connecting',
+        [ReadyState.OPEN]: 'Open',
+        [ReadyState.CLOSING]: 'Closing',
+        [ReadyState.CLOSED]: 'Closed',
+        [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
+    }[readyState]
+
+    const data = {
+        labels: labels,
+        datasets: [
+            {
+                label: 'Power consumption',
+                data: powerConsumption,
+                borderColor: 'rgba(68, 115, 158, 1)',
+                backgroundColor: 'rgba(68, 115, 158, 0.5)',
+            }
+        ]
     }
-  }, [lastMessage])
 
-  useEffect(() => {
-    window.fetch(process.env.REACT_APP_API_URL || "")
-        .then(response => response.json())
-        .then(data => setMessage(data.message))
-    // note: don't forget about error handling, see example on https://reactjs.org/docs/faq-ajax.html
-  }, [setMessage])
-
-  const connectionStatus = {
-    [ReadyState.CONNECTING]: 'Connecting',
-    [ReadyState.OPEN]: 'Open',
-    [ReadyState.CLOSING]: 'Closing',
-    [ReadyState.CLOSED]: 'Closed',
-    [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
-  }[readyState]
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {message}
-        </p>
-        <p>
-            WS connection status: {connectionStatus}
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    {message}
+                </p>
+                <p>
+                    WS connection status: {connectionStatus}
+                </p>
+            </header>
+            <Line data={data}/>
+        </div>
+    );
 }
 
 export default App
